83bce9acae45115c0fff3e30ceb9595085e9ed3f
Dzmitry <skysilvaby@gmail.com>
2019-10-25 18:09:23 +0300

    Initial Commit

---
+++ Books/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Books
+//
+//  Created by Dzmitry on 10/25/19.
+//  Copyright © 2019 Dzmitry Krukov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
